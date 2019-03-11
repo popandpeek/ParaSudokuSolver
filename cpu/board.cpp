0aaee4e793040c1e8adb9e67ba6fe6d0daa368b8
Ben Pittman <bennihana09@yahoo.com>
2019-03-10 17:21:29 -0700

    Update board.cpp

--- cpu/board.cpp
+++ cpu/board.cpp
@@ -20,10 +20,10 @@ public:
 
 	// Array of bool pointers to hold cells for board
 	// 0 item in each array signifies filled or empty, 1-9 signifies filled value or potential value
-	bool **board[BOARD_SIZE];
+	bool **board = (bool **)malloc(BOARD_SIZE * sizeof(bool *));
 	Board() {
 		for (int i = 0; i < BOARD_SIZE; i++) {
-			*board[i] = (bool*)malloc((SUB_BOARD_SIZE + 1) * sizeof(bool));
+			board[i] = (bool*)malloc((SUB_BOARD_SIZE + 1) * sizeof(bool));
 		}
 	};
 
@@ -32,8 +32,8 @@ public:
 	void set_board(int* filled) {
 		for (int i = 0; i < BOARD_SIZE; i++) {
 			if (filled[i] != 0) {
-				*board[i][0] = true;
-				*board[i][filled[i]] = true;
+				board[i][0] = true;
+				board[i][filled[i]] = true;
 			}
 		}
 	}
@@ -41,14 +41,14 @@ public:
 	// sets a cell as 
 	void set_cell(int _row, int _col, int _val) {
 		int board_cell = _row + _col * SUB_BOARD_SIZE;
-		*board[board_cell][0] = true;
+		board[board_cell][0] = true;
 		for (int i = 1; i < SUB_BOARD_SIZE + 1; i++) {
-			if (*board[board_cell][i] == true && i != _val) {
-				*board[board_cell][0] = false;
+			if (board[board_cell][i] == true && i != _val) {
+				board[board_cell][0] = false;
 			}
 		}
-	}	
-	
+	}
+
 	// method for finding potential values for empty cells
 	void annotate_potential_entries() {	
 
@@ -60,9 +60,9 @@ public:
 			}
 			// remove values from set that correspond to filled cells in the row
 			for (int i = row * SUB_BOARD_SIZE; i < (row * SUB_BOARD_SIZE) + SUB_BOARD_SIZE; i++) {
-				if (*board[i][0]) {
+				if (board[i][0]) {
 					for (int j = 1; j < SUB_BOARD_SIZE; j++) {
-						if (*board[i][j]) {
+						if (board[i][j]) {
 							row_vals.erase(j);
 						}
 					}
@@ -72,9 +72,9 @@ public:
 			// Fill potential entries in rows
 			if (!row_vals.empty()) {
 				for (int i = row * SUB_BOARD_SIZE; i < (row * SUB_BOARD_SIZE) + SUB_BOARD_SIZE; i++) {
-					if (!*board[i][0]) {
+					if (!board[i][0]) {
 						for (auto it = row_vals.begin(); it != row_vals.end(); ++it) {
-							*board[i][*it] = true;
+							board[i][*it] = true;
 						}
 					}
 				}
@@ -85,9 +85,9 @@ public:
 			// scan col for filled in values and store in temp set
 			std::set<int> col_vals;
 			for (int i = col; i < col * SUB_BOARD_SIZE - col; i += SUB_BOARD_SIZE) {
-				if (*board[i][0]) {
+				if (board[i][0]) {
 					for (int j = 1; j < SUB_BOARD_SIZE; j++) {
-						if (*board[i][j]) {
+						if (board[i][j]) {
 							col_vals.insert(j);
 						}
 					}
@@ -97,10 +97,10 @@ public:
 			// Fill potential entries in columns
 			if (!col_vals.empty()) {
 				for (int i = col; i < col * BOARD_SIZE - col; i += SUB_BOARD_SIZE) {
-					if (!*board[i][0]) {
+					if (!board[i][0]) {
 						for (auto it = col_vals.begin(); it != col_vals.end(); ++it) {
-							if (*board[i][*it] == true) {
-								*board[i][*it] = false;
+							if (board[i][*it] == true) {
+								board[i][*it] = false;
 							}
 						}
 					}
@@ -108,3 +108,4 @@ public:
 			}
 		}
 	}
+};
