6b7f434ebe2c58ea6ac9514eab408b9ef13feb0c
Ben Pittman <bennihana09@yahoo.com>
2019-03-10 17:16:07 -0700

    Update board.cpp

--- cpu/board.cpp
+++ cpu/board.cpp
@@ -1,4 +1,55 @@
-// method for finding potential values for empty cells
+/*
+** Ben Pittman
+** Greg Smith
+** Calvin Winston Fei
+** Term Project - board.cpp
+** Static class for checking solutions.
+** Assumptions: Assumes valid board size and 1D memory allocation
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <set>
+
+const int BOARD_SIZE = 81;
+const int SUB_BOARD_SIZE = 9;
+
+class Board {
+
+public:
+
+	// Array of bool pointers to hold cells for board
+	// 0 item in each array signifies filled or empty, 1-9 signifies filled value or potential value
+	bool **board[BOARD_SIZE];
+	Board() {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			*board[i] = (bool*)malloc((SUB_BOARD_SIZE + 1) * sizeof(bool));
+		}
+	};
+
+	// Method to set the board according to passed integer array
+	// assumes the filled integer array is of size BOARD_SIZE contains only values between 1 and 9
+	void set_board(int* filled) {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			if (filled[i] != 0) {
+				*board[i][0] = true;
+				*board[i][filled[i]] = true;
+			}
+		}
+	}
+
+	// sets a cell as 
+	void set_cell(int _row, int _col, int _val) {
+		int board_cell = _row + _col * SUB_BOARD_SIZE;
+		*board[board_cell][0] = true;
+		for (int i = 1; i < SUB_BOARD_SIZE + 1; i++) {
+			if (*board[board_cell][i] == true && i != _val) {
+				*board[board_cell][0] = false;
+			}
+		}
+	}	
+	
+	// method for finding potential values for empty cells
 	void annotate_potential_entries() {	
 
 		for (int row = 0; row < SUB_BOARD_SIZE; row++) {
